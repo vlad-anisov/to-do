c6fcdbf96e1702041d8bc44da035170e837d8643
vlad-anisov <anisoffgo@gmail.com>
2020-03-11 23:43:30 +0300

    deploy to heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,3 +141,6 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 ACCOUNT_LOGOUT_ON_GET = True
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
